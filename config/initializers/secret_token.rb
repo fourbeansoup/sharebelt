6aa1209e7b92725017d9740f50c2629446b3171f
Josh Owens <joshua.owens@gmail.com>
2012-10-12 20:05:44 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharebelt::Application.config.secret_token = '07f116bc2cdacc768a09a1e3bbc9b74bd1f503779d48f298b5aa3cad49932671bc73b86408431917b4d50573c87ba0fa134b0c98948a2ca0ebe49f997e06b21f'
