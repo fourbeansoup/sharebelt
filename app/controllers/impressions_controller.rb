636b2e56056a988b12de86832ebc947ddb625320
Josh Owens <joshua.owens@gmail.com>
2012-10-13 21:33:06 -0400

    Support site id in the controller

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -1,7 +1,16 @@
 class ImpressionsController < ApplicationController
+  before_filter :find_site
+
   respond_to :json
+
   def index
     @domain = Rails.env == "development" ? "localhost:3000" : "sharebelt.com"
     @referrer = Referrer.for(params[:referrer],params[:location])
+  end
+
+  protected
+
+  def find_site
+    @site = Site.find(params[:id])
   end
 end
