6a148a67d57753ef9d7d6b3c4e1e10214be66ab8
Josh Owens <joshua.owens@gmail.com>
2012-10-13 20:51:09 -0400

    Add the site based impression route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Sharebelt::Application.routes.draw do
 
-  resources :sites
+  resources :sites do
+    resources :impressions
+  end
 
   match "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
   root to: 'high_voltage/pages#show', id: 'home'
-  resources :impressions
 end
