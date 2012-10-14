0f0f1169c914253cb491b5384fa387e47a371390
Joel Turnbull <joelbywan@gmail.com>
2012-10-14 19:53:26 -0400

    yo

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -4,7 +4,7 @@ class ImpressionsController < ApplicationController
   respond_to :json
 
   def index
-    @domain = Rails.env == "development" ? "localhost:3000" : "sharebelt.com"
+    @domain = "sharebelt.com"
     @referrer = Referrer.for(params[:referrer],params[:location])
     @impression = @site.impressions.create(referring_url: params[:referrer], landing_url: params[:location])
   end
