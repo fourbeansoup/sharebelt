5729142a125c8c8af93e2a0606d4e91cdb9cded6
Joel Turnbull <joelbywan@gmail.com>
2012-10-14 18:33:55 -0400

    real data for chart

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,34 +2,7 @@ class ReportsController < ApplicationController
   respond_to :json
 
   def show
-    x = [
-      {
-        key:"Impressions",
-        values: [
-          {
-            x: Date.today - 1.day,
-            y: 5,
-          },
-          {
-            x: Date.today,
-            y: 10,
-          },
-        ]
-      },
-      {
-        key:"Clicks",
-        values: [
-          {
-            x: Date.today - 1.day,
-            y: 3,
-          },
-          {
-            x: Date.today,
-            y: 2,
-          },
-        ]
-      },
-    ]
-    render :json => x
+    site = Site.find(params[:id])
+    render :json => site.graph_data
   end
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -2,4 +2,27 @@ class Site < ActiveRecord::Base
   belongs_to :user
   has_many :impressions, :dependent => :destroy
   attr_accessible :name
+
+  def graph_data
+    impression_totals = impressions.group(:viewed_on).order(:viewed_on).count
+    click_totals = impressions.where(clicked:true).group(:viewed_on).order(:viewed_on).count
+    impression_values = []
+    click_values = []
+    impression_totals.each do |date,impression_count|
+      click_count = click_totals[date]
+      click_count = 0 unless click_count
+      impression_values << { x: date, y:impression_count }
+      click_values << { x: date, y:click_count }
+    end
+    [
+      {
+        key: "Impressions",
+        values: impression_values,
+      },
+      {
+        key: "Clicks", 
+        values: click_values,
+      },
+    ]
+  end
 end
